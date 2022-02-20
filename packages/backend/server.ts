09194bfd7b3cd8cdf231ad8a78d3065e5aad86bd
Joseph Chen <jchen42703@gmail.com>
2022-02-20 03:17:06 -0500

    Setup a shared package with lerna

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -1,11 +1,24 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+import express from "express";
+import _ from "lodash";
+import { QueryPayload } from "shared-dto";
+
 const app = express();
 const port = 3001;
 
-app.use(cors());
-app.get("/", (req: Request, res: Response) => {
-  res.json({ foo: "bar" });
+app.use((_req, res, next) => {
+  // Allow any website to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Continue to next middleware
+  next();
+});
+
+app.get("/", (_req, res) => {
+  const responseData: QueryPayload = {
+    payload: _.snakeCase("Server data returned successfully"),
+  };
+
+  res.json(responseData);
 });
 
 app.listen(port, () => {
